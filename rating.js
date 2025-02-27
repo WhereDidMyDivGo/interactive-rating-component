35977587f6da9f7d665c9802c472e625d497672e
WhereDidMyDivGo <ahmadgameover3@gmail.com>
2025-02-27 14:09:57 +0400

    Introduced a check to validate the rating value (r) before updating the displayed message.

--- rating.js
+++ rating.js
@@ -3,6 +3,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const r = params.get("r");
   const outOf = document.querySelector(".out-of");
   if (r) {
-    outOf.textContent = `You selected ${r} out of 5`;
+    if ([1, 2, 3, 4, 5].includes(Number(r))) {
+      outOf.textContent = `You selected ${r} out of 5`;
+    } else {
+      outOf.textContent = `Invalid rating`;
+    }
   }
 });
